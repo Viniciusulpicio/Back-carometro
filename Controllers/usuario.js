5074f18aa5971f17cfec4eb074a9ffbc4b4f25a2
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-04-23 16:56:36 -0300

    trecho no Controllers/usuario.js esta quebrando o código

--- Controllers/usuario.js
+++ Controllers/usuario.js
@@ -15,9 +15,9 @@ exports.getById = async (req, res) => {
 exports.createUsuario = async (req,res) => {
     const usuarioCadastrado = await Usuario.findOne({cpf: req.body.cpf});
     // Verificação duplicidade do usuario cadastrado
-    if (usuarioCadastrado) {
-        return res.send("Já existe um usuario cadastrado com esse CPF.")
-    }
+    // if (usuarioCadastrado) {
+    //     return res.send("Já existe um usuario cadastrado com esse CPF.")
+    // }
     const usuarioCriado = await Usuario.create(req.body)
     console.log("usuarioCriado", usuarioCriado)
     return res.send("oi")
